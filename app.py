73ceee927ca5c688fd13890af6a752c28f8aa663
V8CrossPlane <tuongduylinh1999@gmail.com>
2020-09-08 13:48:50 +0700

    Homework_2

--- app.py
+++ app.py
@@ -3,22 +3,45 @@ import jwt
 from functools import wraps
 import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.exceptions import HTTPException
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'randomtext'
+app.config['TRAP_HTTP_EXCEPTIONS']=True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@127.0.0.1/fresher_flask'
 
 db = SQLAlchemy(app)
 
 class user_profile(db.Model):
+    __tablename__='user_profile'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(255), unique=True)
     name = db.Column(db.String(255))
     password = db.Column(db.String(255))
     admin = db.Column(db.Boolean)
+
+class products(db.Model):
+    __tablename__= 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    productCode = db.Column(db.String(15))
+    productName = db.Column(db.String(70))
+    productLine = db.Column(db.String(50))
+    productScale = db.Column(db.String(10))
+    productVendor = db.Column(db.String(50))
+    productDescription = db.Column(db.Text)
+    quantityInStock = db.Column(db.SMALLINT)
+
+class bugs(db.Model):
+    __tablename__ = 'bugs'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(100))
 
     # def __init__(self, public_id, username, password, admin): 
     #     self.public_id = public_id
@@ -44,6 +67,7 @@ def token_required(f):
         
         return f(current_user, *args, **kwargs)
     return decorated
+
 @app.route('/user', methods=['GET'])
 @token_required
 def get_all_user(current_user):
@@ -122,7 +146,7 @@ def delete_user(current_user, public_id):
     db.session.commit()
     return jsonify({'message' : 'the user has been deleted'})
 
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login():
     auth = request.authorization
 
@@ -142,6 +166,31 @@ def login():
 
         return jsonify({'token' : token.decode('UTF-8')})
     return make_response('Could not verify', 401, {'WWW-Authenticate':'Basic realm="Login required"'})
+
+@app.route('/test', methods=['GET'])
+@token_required
+def test(current_user):
+    if not current_user.admin:
+        return jsonify({'message':'cannot perfrom that function'})
+
+    result = db.engine.execute(text("SELECT LEFT(productDescription, 50) FROM products"))
+    names = [row[0] for row in result]
+    return jsonify({'results':names})
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+
+    inputBugToDB = db.engine.execute(text("insert into bugs(code, name, description) values(%s, '%s', '%s')" %(e.code, e.name, e.description)))
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
